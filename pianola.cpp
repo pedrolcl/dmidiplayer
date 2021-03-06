595bbe0011b00d0ab29c67125f00af3173bdca39
Pedro Lopez-Cabanillas <plcl@users.sourceforge.net>
2021-03-06 17:28:15 +0100

    missing include

--- pianola.cpp
+++ pianola.cpp
@@ -22,6 +22,7 @@
 #include <QLabel>
 #include <QMenuBar>
 #include <QAction>
+#include <QCloseEvent>
 #include <drumstick/settingsfactory.h>
 #include <drumstick/pianokeybd.h>
 #include <drumstick/rtmidioutput.h>
