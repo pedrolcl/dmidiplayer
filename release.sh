8dd95093ba56812e151e3b0804f9aff3ce170df0
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2021-06-22 18:12:37 +0200

    release preparations

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REL=1.3.1
+REL=1.4.0
 git archive --format=zip --prefix=dmidiplayer-${REL}/ --output build/dmidiplayer-${REL}.zip master
 git archive --format=tar --prefix=dmidiplayer-${REL}/ master | gzip > build/dmidiplayer-${REL}.tar.gz
 git archive --format=tar --prefix=dmidiplayer-${REL}/ master | bzip2 > build/dmidiplayer-${REL}.tar.bz2
