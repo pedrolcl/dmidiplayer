32194a00dfbcff5fe93342e9db2d8f73b552644b
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2023-01-07 17:01:43 +0100

    copyright year updated

--- settings.h
+++ settings.h
@@ -1,6 +1,6 @@
 ﻿/*
     Drumstick MIDI File Player Multiplatform Program
-    Copyright (C) 2006-2022, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2006-2023, Pedro Lopez-Cabanillas <plcl@users.sf.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
