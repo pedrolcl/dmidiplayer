33cacc3f56a6f2872cc16f453848faac514d03dd
Pedro López-Cabanillas <plcl@users.sourceforge.net>
2024-08-20 12:21:16 +0200

    copyright years updated

--- events.cpp
+++ events.cpp
@@ -1,6 +1,6 @@
 /*
     Drumstick MIDI File Player Multiplatform Program
-    Copyright (C) 2006-2023, Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2006-2024, Pedro Lopez-Cabanillas <plcl@users.sf.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
--- playlist.h
+++ playlist.h
@@ -1,6 +1,6 @@
 ﻿/*
     Drumstick MIDI/Karaoke Player
-    Copyright (C) 2009-2023 Pedro Lopez-Cabanillas <plcl@users.sf.net>
+    Copyright (C) 2009-2024 Pedro Lopez-Cabanillas <plcl@users.sf.net>
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
